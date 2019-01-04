6daafe692231f0e74e60911f33de6b43a0273476
lrajah <geviron@msn.com>
2019-01-04 11:41:47 +0100

    Implementation methode asHTML

---
+++ src/interface/htmlInterface.ts
@@ -0,0 +1,13 @@
+/**
+ * @name htmlInterface
+ * @desc Définition des methodes de rendu HTML
+ * @author LKueln
+ * @pakage interface
+ * @version 1.0.0 
+ */
+export interface htmlInterface{
+    /**
+     * Affiche un contenu dans la fenetre du navigateur
+     */
+    asHTML():void;
+}
